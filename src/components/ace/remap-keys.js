a01daab5d71cb3c50652675e2f38964e78cb0e6a
evgeny <khokhlov.e.n@gmail.com>
2020-06-10 00:43:36 +0300

    feat: add eval command

--- src/components/ace/remap-keys.js
+++ src/components/ace/remap-keys.js
@@ -1,3 +1,5 @@
+import { Range } from 'ace-builds';
+
 const keyBindings = [
     {
         bindKey: { linux: 'ctrl-shift-l', mac: 'command-shift-l', win: 'ctrl-shift-l' },
@@ -28,6 +30,7 @@ const remapKeys = editor => {
 
     editor.commands.addCommand({
         name: 'beautify',
+        description: 'Beautify code',
         bindKey: { win: 'ctrl-alt-l', mac: 'command-alt-l' },
         exec: editor => {
             const code = editor.session.getValue();
@@ -42,11 +45,40 @@ const remapKeys = editor => {
 
     editor.commands.addCommand({
         name: 'enumerate',
+        description: 'Insert line number',
         bindKey: { win: 'ctrl-shift-p', mac: 'command-shift-p' },
         multiSelectAction: 'forEach',
         exec: editor => {
             const { row } = editor.selection.lead;
             editor.insert(`${row + 1}`);
+        }
+    });
+
+    editor.commands.addCommand({
+        name: 'eval',
+        description: 'Eval expression',
+        bindKey: { win: 'ctrl-j', mac: 'command-j' },
+        multiSelectAction: 'forEach',
+        scrollIntoView: 'cursor',
+        exec: editor => {
+            const range = editor.getSelectionRange();
+
+            if (editor.selection.isEmpty()) return;
+
+            const text = editor.session.getTextRange(range);
+
+            try {
+                // eslint-disable-next-line no-eval
+                const evaled = eval(text);
+                const evaledString = String(evaled);
+                editor.session.replace(range, evaledString);
+                const { start } = range;
+                editor.selection.setSelectionRange(
+                    new Range(start.row, start.column, start.row, start.column + evaledString.length)
+                );
+            } catch (e) {
+                // do nothing
+            }
         }
     });
 };
